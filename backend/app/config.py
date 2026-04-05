e7009ce03672e72082882c772339c9600f376a24
shashank410 <shashanksolanki020@gmail.com>
2026-04-05 12:16:10 +0530

    Switch to HF API embeddings

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+HF_TOKEN = os.getenv("HF_TOKEN")
--- backend/app/core/embeddings.py
+++ backend/app/core/embeddings.py
@@ -1,7 +1,9 @@
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
+from app.config import HF_TOKEN
 
 def get_embeddings():
-    embeddings = HuggingFaceEmbeddings(
-        model_name="all-MiniLm-L6-v2"
+    embeddings = HuggingFaceEndpointEmbeddings(
+        model="sentence-transformers/all-MiniLM-L6-v2",
+        huggingfacehub_api_token=HF_TOKEN
     )
     return embeddings
